f2c475cb5036c37bb36de517aab5431f3bdb7b3f
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-09-16 08:43:57 -0400

    Update alert.js

--- JS/alert.js
+++ JS/alert.js
@@ -1,1 +1,2 @@
 alert("Welcome! This website was made by Payton Brannen and Phillip Barnes, you may not copy/steal this.")
+console.log("This website was made by Payton Brannen and Phillip Barnes, you may not copy/steal this.")
